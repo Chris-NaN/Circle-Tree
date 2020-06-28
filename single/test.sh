b6ccb817f825473b908f3c5ec7b90a511db46be4
Kakarot007 <646257316@qq.com>
2020-06-28 19:31:39 +0800

    change shell

--- single/test.sh
+++ single/test.sh
@@ -4,11 +4,15 @@ size=1000000
 output_file="output.txt"
 #echo "FAST-FAIR" > output.txt
 #./FAST-FAIR -i $input_file -n $size >> output.txt
-echo "FAST-FAIR_bufer:"  > output.txt
-./FAST-FAIR_buffer -i $input_file -n $size >> output.txt
+#echo "FAST-FAIR_bufer:"  > output.txt
+#./FAST-FAIR_buffer -i $input_file -n $size >> output.txt
 #echo "Circle-Tree" >> output.txt
 #./Circle-Tree -i $input_file -n $size >> output.txt
 #echo "Circle-Tree_buffer" >> output.txt
 #./Circle-Tree_buffer -i $input_file -n $size >> output.txt
 #echo "FP-Tree" >> output.txt
 #./FP-Tree -i $input_file -n $size >> output.txt
+echo "B+Tree" > output.txt
+./B+Tree -i $input_file -n $size >> output.txt
+echo "B+Tree_binary" >> output.txt
+./B+Tree_binary -i $input_file -n $size >> output.txt
