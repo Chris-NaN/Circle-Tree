305a524c35ca677566254d118838196f499d6feb
Leon <646257316@qq.com>
2020-06-30 15:51:57 +0800

    add FAST-FAIR-fp

--- single/test.sh
+++ single/test.sh
@@ -2,17 +2,19 @@
 input_file="test/random_100m_input.txt"
 size=1000000
 output_file="output.txt"
-#echo "FAST-FAIR" > output.txt
-#./FAST-FAIR -i $input_file -n $size >> output.txt
-#echo "FAST-FAIR_bufer:"  > output.txt
-#./FAST-FAIR_buffer -i $input_file -n $size >> output.txt
-#echo "Circle-Tree" >> output.txt
-#./Circle-Tree -i $input_file -n $size >> output.txt
-#echo "Circle-Tree_buffer" >> output.txt
-#./Circle-Tree_buffer -i $input_file -n $size >> output.txt
-#echo "FP-Tree" >> output.txt
-#./FP-Tree -i $input_file -n $size >> output.txt
-echo "B+Tree" > output.txt
-./B+Tree -i $input_file -n $size >> output.txt
-echo "B+Tree_binary" >> output.txt
-./B+Tree_binary -i $input_file -n $size >> output.txt
+echo "FAST-FAIR" > output.txt
+./FAST-FAIR -i $input_file -n $size >> output.txt
+echo "FAST-FAIR_bufer:"  >> output.txt
+./FAST-FAIR_buffer -i $input_file -n $size >> output.txt
+echo "FAST-FAIR_fp:"  >> output.txt
+./FAST-FAIR_fp -i $input_file -n $size >> output.txt
+echo "Circle-Tree" >> output.txt
+./Circle-Tree -i $input_file -n $size >> output.txt
+echo "Circle-Tree_buffer" >> output.txt
+./Circle-Tree_buffer -i $input_file -n $size >> output.txt
+echo "FP-Tree" >> output.txt
+./FP-Tree -i $input_file -n $size >> output.txt
+#echo "B+Tree" > output.txt
+#./B+Tree -i $input_file -n $size >> output.txt
+#echo "B+Tree_binary" >> output.txt
+#./B+Tree_binary -i $input_file -n $size >> output.txt
